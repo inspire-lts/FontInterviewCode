2c9c96a53ad9bca930354738e5e7eabe29cf4f0a
inspire-lts <1183132919@qq.com>
2023-11-23 22:58:57 +0800

    feat:完善深拷贝

--- 深拷贝.js
+++ 深拷贝.js
@@ -30,5 +30,47 @@ function deepObj1(obj,  map = new WeakMap()) {
   }
 }
 
-let test = [1, 2, {a: 2}]
-console.log(deepObj(test))
+function deepObj2(obj, map = new WeakMap()) {
+  if (map.get(obj)) return obj;
+
+  if (obj instanceof Map) {
+    const newMap = new Map();
+    map.set(obj, true);
+
+    obj.forEach((value, key) => {
+      newMap.set(key, deepObj2(value, map));
+    });
+
+    return newMap;
+  }
+
+  if (obj instanceof Set) {
+    const newSet = new Set();
+    map.set(obj, true);
+
+    obj.forEach(value => {
+      newSet.add(deepObj2(value, map));
+    });
+
+    return newSet;
+  }
+
+  if (typeof obj === "object" && obj !== null) {
+    map.set(obj, true);
+    let result = Array.isArray(obj) ? [] : {};
+
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result[key] = deepObj2(obj[key], map);
+      }
+    }
+
+    return result;
+  } else {
+    return obj;
+  }
+}
+
+
+let test = [1, 2, {a: 2}, new Set([1, 2, 3])]
+console.log(deepObj2(test))
